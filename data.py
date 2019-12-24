53bd1a7e79b177024dedb7bc02d1e2e9e73a98d8
CamClrt <camille.clarret@gmail.com>
2019-12-24 21:55:05 +0100

    Init project

---
+++ data.py
@@ -0,0 +1,20 @@
+"""This file contain the required datas for the programm"""
+
+#labyrinth pattern
+
+labyrinth = [
+    [0,x,x,x,x,x,x,x,x,x,0,x,x,0,0],
+    [0,0,0,0,x,x,x,x,x,x,0,0,0,0,x],
+    [x,x,x,0,x,0,0,0,0,0,0,x,x,0,x],
+    [x,x,x,0,x,0,x,0,x,0,x,x,x,0,x],
+    [x,x,x,0,x,0,x,0,x,0,x,0,x,0,x],
+    [x,0,0,0,x,x,x,0,x,0,x,0,0,0,x],
+    [x,0,x,0,0,0,0,0,x,0,x,x,x,x,x],
+    [x,0,x,x,0,x,x,0,x,0,0,0,x,x,0],
+    [x,0,x,x,0,x,x,0,x,x,x,0,x,0,0],
+    [x,x,x,x,0,x,x,0,0,0,x,0,0,0,x],
+    [x,0,0,0,0,0,x,x,x,x,x,x,x,x,x],
+    [x,0,x,x,x,x,x,0,0,0,x,0,0,0,0],
+    [x,0,x,0,0,0,0,0,x,0,0,0,x,0,x],
+    [x,0,0,0,x,x,x,0,x,x,x,x,x,0,x],
+    [x,x,x,0,x,x,x,0,0,0,0,x,x,0,x]]
