c7a0c01cb94c1a68d205e28698288ac385e2a238
CamClrt <camille.clarret@gmail.com>
2020-01-13 10:08:53 +0100

    optimize main code and update methode in Player class

--- Main.py
+++ Main.py
@@ -6,11 +6,8 @@ map_list = [] #contain the matrix of the map
 player_name = "" #determine the name of the player
 #items_collected_position = "" #store the items' position collected
 #items_counter = set() #determine the number of items collected
-player_position = "" #pygame object : rect
 close_window = False
 game_condition = False
-x_player = 0 #collect the abscissa of the player
-y_player = 0 #collect the ordinate of the wall
 x_wall = 0 #collect the abscissa of the wall
 y_wall = 0 #collect the ordinate of the wall
 x_item = 0 #collect the abscissa of the item
@@ -25,53 +22,37 @@ player, labyrinth, items_positions, map_list = init_game(PLAYER_NAME)
 #init pygame
 pygame.init()
 
-#set the window and its title
+#set the empty window and its title
 pygame.display.set_caption(WINDOW_TITLE) #determine the title
 window = pygame.display.set_mode((WINDOW_SIZE,WINDOW_SIZE)) #determine the size of the window
 background = pygame.image.load(BACKGROUND).convert() #load the background
 window.blit(background, (0,0)) #stick the background
 
-#set the player on the map
-player_picture = pygame.image.load(PLAYER_PICTURE_URL).convert_alpha() #load the player image
-player_position = player_picture.get_rect() #init rect (or move)
-window.blit(player_picture, player_position) #stick the player
-
-#set the guard on the map
-guard_picture = pygame.image.load(GUARD_PICTURE_URL).convert()
-
-#set a part of the wall on the map
-wall = pygame.image.load(WALL_PICTURE_URL)
-
 #refresh the screen
 pygame.display.flip()
 
-#limitation de vitesse de la boucle
+#limit the FPS
 pygame.time.Clock().tick(1) #FPS > notion à revoir frame per second
 
-# move the player with the down, up, left and right buttons and quit the game
+#move the player with the down, up, left and right buttons and quit the game
 while close_window == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  #quit the programe
             close_window = True
         elif event.type == KEYDOWN and event.key == K_DOWN:
-            res = player.goDown(*map_list)
-            if res == "Path":
-                player_position = player_position.move(0, SPRITE_SIZE)
+            player.go_down(*map_list)
         elif event.type == KEYDOWN and event.key == K_UP:
-            res = player.goUp(*map_list)
-            if res == "Path":
-                player_position = player_position.move(0, -SPRITE_SIZE)
+            player.goUp(*map_list)
         elif event.type == KEYDOWN and event.key == K_LEFT:
-            res = player.goLeft(*map_list)
-            if res == "Path":
-                player_position = player_position.move(-SPRITE_SIZE,0)
+            player.goLeft(*map_list)
         elif event.type == KEYDOWN and event.key == K_RIGHT:
-            res = player.goRight(*map_list)
-            if res == "Path":
-                player_position = player_position.move(SPRITE_SIZE, 0)
+            player.goRight(*map_list)
 
         #stick the background on the map
         window.blit(background,(0,0))
+
+        #set a part of the wall on the map
+        wall = pygame.image.load(WALL_PICTURE_URL)
 
         #stick the wall on the map
         for line in range(len(map_list)):
@@ -88,9 +69,13 @@ while close_window == False:
             x_item, y_item = items_positions[element]
             window.blit(pygame.transform.scale(item, (40, 40)), (x_item * SPRITE_SIZE, y_item * SPRITE_SIZE))
 
-        #stick the player and the guard on the map
+        #set and stick the player on the map
+        player_picture = pygame.image.load(PLAYER_PICTURE_URL).convert_alpha()  #load the player image
+        window.blit(player_picture, player.get_position())  #stick the player
+
+        #set and stick the guard on the map
+        guard_picture = pygame.image.load(GUARD_PICTURE_URL).convert()
         window.blit(guard_picture, FINISH_PX)
-        window.blit(player_picture, player_position)
 
         #refresh the window
         pygame.display.flip()
@@ -100,9 +85,7 @@ while close_window == False:
 
 
         #win and close if the player reach the guard
-        x_player = player_position.x / SPRITE_SIZE
-        y_player = player_position.y / SPRITE_SIZE
-        if (x_player,y_player) == FINISH:
+        if player.get_position() == FINISH_PX: #si position player == position garde > A CORRIGER
             close_window = True
 
 
--- Player.py
+++ Player.py
@@ -1,3 +1,5 @@
+from data import SPRITE_SIZE
+
 class Player():
     """to create the player"""
 
@@ -9,63 +11,43 @@ class Player():
         self.y = y #ordinate
 
     #METHODES
-    def goRight(self,*list):
+    def go_right(self,*list):
         """try to move to right"""
-        res = ""
         self.x += 1
-        res = "Path"
         try:
             list[self.y][self.x]
         except IndexError:
             self.x -= 1 #cancel the move
-            res = "Wall"
         if list[self.y][self.x] == "x":
             self.x -= 1 #cancel the move
-            res = "Wall"
-        return res
 
-    def goLeft(self,*list):
+    def go_left(self,*list):
         """try to move to left"""
-        res = ""
         self.x -= 1
-        res = "Path"
         if self.x < 0:
             self.x += 1 #cancel the move
-            res = "Wall"
         if list[self.y][self.x] == "x":
             self.x += 1 #cancel the move
-            res = "Wall"
-        return res
 
-    def goUp(self,*list):
+    def go_up(self,*list):
         """try to move to up"""
-        res = ""
         self.y -= 1
-        res = "Path"
         if self.y < 0:
             self.y += 1 #cancel the move
-            res = "Wall"
         if list[self.y][self.x] == "x":
             self.y += 1 #cancel the move
-            res = "Wall"
-        return res
 
-    def goDown(self,*list):
+    def go_down(self,*list):
         """try to move to down"""
-        res = ""
         self.y += 1
-        res = "Path"
         try:
             list[self.y][self.x]
         except IndexError:
             self.y -= 1 #cancel the move
-            res = "Wall"
         if list[self.y][self.x] == "x":
             self.y -= 1 #cancel the move
-            res = "Wall"
-        return res
 
     def get_position(self):
         """return the current position of player"""
-        position = (self.x,self.y)
+        position = (self.x * SPRITE_SIZE,self.y * SPRITE_SIZE)
         return position
